1dc9163071d2b7261d7bce30b3dff6bfe70dff39
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-12-17 23:23:08 -0700

    able to read program from external file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -22,15 +22,29 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = [0] * 256
+            # # From print8.ls8
+            # 0b10000010, # LDI R0,8
+            # 0b00000000,
+            # 0b00001000,
+            # 0b01000111, # PRN R0
+            # 0b00000000,
+            # 0b00000001, # HLT
+        
+
+        filename = sys.argv[1]
+        address = 0
+
+        with open(filename) as f:
+            for line in f:
+                line = line.split('#')
+                val = int(line[0], 2)
+                program[address] = val
+                # print(program, val)
+                address += 1
+
+        print(program[:20])
+        sys.exit()
 
         for instruction in program:
             self.ram[address] = instruction
